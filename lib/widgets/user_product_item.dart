c599f3e98192edfbc8224922ebc513bbfb7d1ff0
Maya <melokitty.1003@gmail.com>
2020-11-24 10:48:45 +0900

    add delete method

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -29,6 +31,8 @@ class UserProductItem extends StatelessWidget {
           IconButton(
               icon: Icon(Icons.delete),
               onPressed: () {
+                // delete product
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
               },
               color: Theme.of(context).errorColor,
           )
