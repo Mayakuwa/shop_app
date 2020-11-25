4e45090c9c1212011b4e42cc3961909290d1c6da
Maya <melokitty.1003@gmail.com>
2020-11-25 11:34:07 +0900

    add to pull to refresh in  user_products_screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -9,6 +9,10 @@ class UserProductsScreen extends StatelessWidget {
 
   static const routeName  = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+   await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsDate = Provider.of<Products>(context);
@@ -23,19 +27,22 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsDate.items.length,
-          itemBuilder: (_, i) =>  Column(
-            children: [UserProductItem(
-              productsDate.items[i].id,
-              productsDate.items[i].title,
-              productsDate.items[i].imageUrl,
-            ),
-            Divider()
-            ],
-          )
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsDate.items.length,
+            itemBuilder: (_, i) =>  Column(
+              children: [UserProductItem(
+                productsDate.items[i].id,
+                productsDate.items[i].title,
+                productsDate.items[i].imageUrl,
+              ),
+              Divider()
+              ],
+            )
+          ),
         ),
       ),
     );
