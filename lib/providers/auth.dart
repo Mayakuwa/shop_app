cadc9bd65e499057744694a59f907cdcb10de792
Maya <melokitty.1003@gmail.com>
2020-11-27 11:52:03 +0900

    add error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -10,15 +12,23 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(String email, String password, String urlSegment) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBw9RdXaurXOnlB-JtrPcVQHHvysKxarIQ';
-    final response = await http.post(
-        url,
-        body: json.encode(
-            {
-              'email':email,
-              'password': password,
-              'returnSecureToken': true
-            }));
-    print(json.decode(response.body));
+
+    try {
+      final response = await http.post(
+          url,
+          body: json.encode(
+              {
+                'email':email,
+                'password': password,
+                'returnSecureToken': true
+              }));
+      final responseDate = json.decode(response.body);
+      if (responseDate['error'] != null) {
+        throw HttpException(responseDate['error']['message']);
+      }
+    } catch (error){
+      throw error;
+    }
   }
 
   Future<void> singUp (String email, String password) async {
