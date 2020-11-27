2a2c27bf37a89ca7c72d23a05f46bffcccc22f5a
Maya <melokitty.1003@gmail.com>
2020-11-27 11:25:52 +0900

    change singnup and longin logic

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,30 +7,26 @@ class Auth with ChangeNotifier {
   DateTime _expireDate;
   String _userId;
 
-  Future<void> singUp (String email, String password) async {
-    const url =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBw9RdXaurXOnlB-JtrPcVQHHvysKxarIQ';
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBw9RdXaurXOnlB-JtrPcVQHHvysKxarIQ';
     final response = await http.post(
         url,
         body: json.encode(
-          {
-            'email':email,
-            'password': password,
-            'returnSecureToken': true
-    }));
+            {
+              'email':email,
+              'password': password,
+              'returnSecureToken': true
+            }));
     print(json.decode(response.body));
   }
 
-  Future<void> login(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBw9RdXaurXOnlB-JtrPcVQHHvysKxarIQ';
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email':email,
-        'password':password,
-        'returnSecureToken' : true
-      }));
-    print(json.decode(response.body));
+  Future<void> singUp (String email, String password) async {
+    return _authenticate(email, password, 'signUp');
   }
+
+  Future<void> login(String email, String password) async {
+   return _authenticate(email, password, 'signInWithPassword');
+  }
+
 }
