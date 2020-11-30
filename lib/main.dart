6e6701382e74d95c82518f737b2a59657a468bbd
Maya <melokitty.1003@gmail.com>
2020-11-30 15:22:00 +0900

    add logout autologin function

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shop_app/screens/auth_screen.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
+import 'package:shop_app/screens/splash_screen.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
 import 'screens/products_overview_screen.dart';
 import 'screens/product_detail_screen.dart';
@@ -53,7 +54,15 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: auth.isAuth ? ProductOverViewScreen() :  AuthScreen(),
+        home: auth.isAuth
+            ? ProductOverViewScreen()
+            : FutureBuilder(
+              future: auth.tryAutoLogin(),
+              builder: (ctx, authResultSnapshot) =>
+                authResultSnapshot.connectionState == ConnectionState.waiting
+                    ? SplashScreen()
+                    : AuthScreen()
+            ),
         routes: {
           //ルート設定
           ProductDetailScreen.routeName:  (ctx) => ProductDetailScreen(),
