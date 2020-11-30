e82b762d15d86fa97774b77997d7623ad9b5b578
Maya <melokitty.1003@gmail.com>
2020-11-30 14:02:36 +0900

    add order function to each users

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class MyApp extends StatelessWidget {
           create: null,
           update: (ctx, auth, previousOrders) => Orders(
             auth.token,
+            auth.userId,
             previousOrders == null ? [] : previousOrders.orders
           ),
         ),
--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -20,15 +20,16 @@ class OrderItem {
 class Orders with ChangeNotifier {
   List<OrderItem> _orders =[];
   final String authToken;
+  final String userId;
 
-  Orders(this.authToken, this._orders);
+  Orders(this.authToken, this.userId, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
   Future<void> fetchAndSetOrder() async {
-    final url = 'https://my-shop-388ad.firebaseio.com/orders.json?auth=$authToken';
+    final url = 'https://my-shop-388ad.firebaseio.com/orders/$userId.json?auth=$authToken';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     //dynamicで宣言すると、動的な型宣言が可能となる
@@ -57,7 +58,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cardProducts, double  total) async {
-    final url = 'https://my-shop-388ad.firebaseio.com/orders.json?auth=$authToken';
+    final url = 'https://my-shop-388ad.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timeStamp = DateTime.now();
     final response = await http.post(
         url,
