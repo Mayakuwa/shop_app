40fb8cea299e1a21b2cc53b33ab049707238a7d7
Maya <melokitty.1003@gmail.com>
2020-12-01 13:26:53 +0900

    add custom transition_theme

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -18,43 +18,53 @@ class ProductDetailScreen extends StatelessWidget {
         context,
         listen: false).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: 300,
-              child: Hero(
-                tag: loadProduct.id,
-                child: Image.network(
-                  loadProduct.imageUrl,
-                  fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(loadProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+                title: Text(loadProduct.title),
+                background: Hero(
+                  tag: loadProduct.id,
+                  child: Image.network(
+                    loadProduct.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
             ),
-            SizedBox(height: 10),
-            Text(
-              '\$${loadProduct.price}',
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              child: Text(
-                loadProduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
-              ),
-            )
-          ]
-        ),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate(
+               [
+                SizedBox(height: 10),
+                Text(
+                  '\$${loadProduct.price}',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  child: Text(
+                    loadProduct.description,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ),
+                 SizedBox(
+                   height: 800,
+                 )
+              ])
+          )
+        ],
       ),
     );
   }
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -3,6 +3,7 @@ import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
 import 'package:shop_app/providers/auth.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -27,6 +28,10 @@ class AppDrawer extends StatelessWidget {
           title: Text('Orders'),
           onTap: () {
             Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
+            // Navigator.of(context).pushReplacement(
+            //   CustomRoute(
+            //     builder: (ctx) => OrdersScreen(),
+            // ),);
           },
         ),
         Divider(),
